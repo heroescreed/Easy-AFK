00e99622f049b6e5bc831ae202d9c81ad8b4873d
heroescreed <100536226+heroescreed@users.noreply.github.com>
2024-08-21 20:17:21 +0100

    Updated ConfigManager to use lombok constructor

--- easy-afk/src/main/java/com/heroescreed/managers/ConfigManager.java
+++ easy-afk/src/main/java/com/heroescreed/managers/ConfigManager.java
@@ -2,14 +2,13 @@ package com.heroescreed.managers;
 
 import com.heroescreed.Plugin;
 
+import lombok.RequiredArgsConstructor;
+
 import org.bukkit.ChatColor;
 
+@RequiredArgsConstructor
 public class ConfigManager {
     private final Plugin plugin;
-
-    public ConfigManager(Plugin plugin) {
-        this.plugin = plugin;
-    }
 
     public String getMessage(String messagepath){
         return ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages." + messagepath)); // Get the message content from the constants file and add the correct colours
