16cfcf0827881340408357848a4a789ace8b2328
heroescreed <100536226+heroescreed@users.noreply.github.com>
2024-08-22 00:32:50 +0100

    add new settings to config and configmanager

--- easy-afk/src/main/java/com/heroescreed/managers/ConfigManager.java
+++ easy-afk/src/main/java/com/heroescreed/managers/ConfigManager.java
@@ -13,4 +13,12 @@ public class ConfigManager {
     public String getMessage(String messagepath){
         return ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messages." + messagepath)); // Get the message content from the constants file and add the correct colours
     }
+
+    public boolean getSetting(String settingpath){
+        return plugin.getConfig().getBoolean("settings." + settingpath);
+    }
+
+    public int getOption(String optionpath){
+        return plugin.getConfig().getInt("options." + optionpath);
+    }
 }
